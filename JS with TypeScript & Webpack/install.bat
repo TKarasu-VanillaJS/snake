d6e701f0d45ed56a135cce50ead42c301cf73852
Tan KARASU <karasutan@gmail.com>
2020-02-23 23:39:38 +0100

    projet de base

---
+++ JS with TypeScript & Webpack/install.bat
@@ -0,0 +1,1 @@
+npm install --save-dev babel-core babel-loader babel-preset-env css-loader mini-css-extract-plugin webpack webpack-cli webpack-dev-server
---
+++ JS with TypeScript & Webpack/webpackRun.bat
@@ -0,0 +1,6 @@
+echo 1-creation des dossiers locaux et des fichiers initiaux
+touch webpack.config.js && mkdir assets src public &&  cd ./src && touch index.js && cd .. && cd ./public && touch  index.html style.css && cd ..  && cd ./assets && mkdir fonts icons images stylesheets && cd ./stylesheets && mkdir libs && touch style.scss && cd ../..
+
+echo 2-initialisation NPM
+npm init
+
