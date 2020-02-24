4c78b24354db0e13574413d4f8ddaa23895d0eb2
Tan KARASU <karasutan@gmail.com>
2020-02-24 10:37:12 +0100

    base ok pour TS WP

--- JS with TypeScript & Webpack/webpack.config.js
+++ JS with TypeScript & Webpack/webpack.config.js
@@ -1,6 +1,19 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
+  entry: {
+    main: "./src/index.js"
+  },
+  mode: "production",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../JS with TypeScript & Webpack/dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase :"dist"
+  },
   watch: true,
   plugins: [new MiniCssExtractPlugin()],
   devtool: "inline-source-map",
